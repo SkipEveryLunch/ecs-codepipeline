1872d98e12decad4bd79d7fa9930754ab95b5ddc
SkipEveryLunch <skipeverylunch@outlook.jp>
2023-10-29 18:32:21 +0900

    change db into MySQL

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_10_29_090513) do
-  create_table "greets", force: :cascade do |t|
+  create_table "greets", charset: "latin1", force: :cascade do |t|
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
