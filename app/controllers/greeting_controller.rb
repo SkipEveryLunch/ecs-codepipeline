efdd43cf113f4e78b65e7aae2980ef9847458ca3
SkipEveryLunch <skipeverylunch@outlook.jp>
2023-10-29 18:59:33 +0900

    change the structure of json

--- app/controllers/greeting_controller.rb
+++ app/controllers/greeting_controller.rb
@@ -1,6 +1,6 @@
 class GreetingController < ApplicationController
   def random_greeting
     greetings = Greet.pluck(:message)
-    render json: { greeting: greetings.sample }
+    render json: { message: greetings.sample }
   end
 end
