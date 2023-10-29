0fb45b70cb2f19c960864aaa3429f6d1e7464781
SkipEveryLunch <skipeverylunch@outlook.jp>
2023-10-29 20:32:04 +0900

    /api/hello ルートを追加

--- app/controllers/greeting_controller.rb
+++ app/controllers/greeting_controller.rb
@@ -1,4 +1,7 @@
 class GreetingController < ApplicationController
+  def hello
+    render json: { greeting: 'hello' }
+  end
   def random_greeting
     greetings = Greet.pluck(:message)
     render json: { greeting: greetings.sample }
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get '/api/hello', to: 'greeting#hello'
   get '/api/greeting', to: 'greeting#random_greeting'
 end
